b86853840584042740d74e94e1cf59a608c7d95e
Loyc <lordloyc@gmail.com>
2023-12-03 21:37:19 -0500

    minor

--- sources/main.c
+++ sources/main.c
@@ -12,13 +12,12 @@
 
 
 #define FNP		(void**)&
-void nullFree	(void **ptrloc) { free( *ptrloc ); *ptrloc = 0; }
+void nullFree	(void **ptrloc){ free( *ptrloc ); *ptrloc = 0; }
 
 //#define FNA			(void***)&
-//void nullFreeArray	(void ***aploc, int size) { for (int i = 0; i < size; i++) { nullFree( aploc[i] ); } }
+//void nullFreeArray	(void ***aploc, int size){ for (int i = 0; i < size; i++){ nullFree( aploc[i] ); }}
 
 Camera	*initPlayerCam();
-
 
 void	refreshView( Camera *camera );
 void	drawGeometry( Camera *camera );
@@ -53,6 +52,8 @@ int main(void)
 
 	CloseWindow();
 
+	nullFree( FNP camera );
+
 	return 0;
 }
 
@@ -69,32 +70,19 @@ Camera *initPlayerCam()
 	camera->fovy		= 90.0f;
 	camera->projection	= CAMERA_PERSPECTIVE;
 
-	camera->position	= ( Vector3 ) { -10.0f, -10.0f, -10.0f };
-	camera->target		= ( Vector3 ) { 0.0f, 0.0f, 0.0f };
-	camera->up			= ( Vector3 ) { 0.0f, 1.0f, 0.0f };
+	camera->position	= ( Vector3 ){ -10.0f, -10.0f, -10.0f };
+	camera->target		= ( Vector3 ){ 0.0f, 0.0f, 0.0f };
+	camera->up			= ( Vector3 ){ 0.0f, 1.0f, 0.0f };
 
 	return camera;
 }
-/*
 
-{
-	Color *colours = calloc( MAX_COLUMNS + 1, sizeof( Color ) );
-
-	for (int i = 0; i < MAX_COLUMNS; i++) { colours[i] = (Color)
-	{
-		GetRandomValue( 64, 255 ),
-		GetRandomValue( 64, 255 ),
-		GetRandomValue( 64, 255 ), 255
-	}; }
-	return colours;
-}
-*/
 // This new camera function allows custom movement/rotation values to be directly provided
 // as input parameters, with this approach, rcamera module is internally independent of raylib inputs
 void	refreshView( Camera *camera )
 {
 	int run = 1;
-	if( IsKeyDown( KEY_LEFT_CONTROL )) { run = 2; }
+	if( IsKeyDown( KEY_LEFT_CONTROL )){ run = 2; }
 
 	UpdateCameraPro( camera,
 		(Vector3){
@@ -107,7 +95,7 @@ void	refreshView( Camera *camera )
 			(IsKeyDown( KEY_SPACE ) || IsKeyDown( KEY_RIGHT_CONTROL ))	* MOV_FACTOR * run -	// Move up-down
 			(IsKeyDown( KEY_LEFT_SHIFT ) || IsKeyDown( KEY_KP_0 ))		* MOV_FACTOR * run
 		},
-		(Vector3){
+		( Vector3 ){
 			GetMouseDelta().x * ROT_FACTOR,	// Rotation: yaw
 			GetMouseDelta().y * ROT_FACTOR,	// Rotation: pitch
 			0.0f							// Rotation: roll
@@ -119,27 +107,33 @@ void drawFace( Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4, Color color )
 {
 	DrawTriangle3D( v1, v2, v3, color );
 	DrawTriangle3D( v1, v3, v4, color );
+
+	DrawLine3D( v1, v2, BLACK );
+	DrawLine3D( v1, v3, BLACK );
+	DrawLine3D( v1, v4, BLACK );
+	DrawLine3D( v2, v3, BLACK );
+	DrawLine3D( v3, v4, BLACK );
 }
 
 void drawVoxel( Vector3 pos)
 {
-	Vector3 p000 = { pos.x + 0.0f, pos.y + 0.0f, pos.z + 0.0f };
-	Vector3 p001 = { pos.x + 0.0f, pos.y + 0.0f, pos.z + 1.0f };
-	Vector3 p010 = { pos.x + 0.0f, pos.y + 1.0f, pos.z + 0.0f };
-	Vector3 p011 = { pos.x + 0.0f, pos.y + 1.0f, pos.z + 1.0f };
-	Vector3 p100 = { pos.x + 1.0f, pos.y + 0.0f, pos.z + 0.0f };
-	Vector3 p101 = { pos.x + 1.0f, pos.y + 0.0f, pos.z + 1.0f };
-	Vector3 p110 = { pos.x + 1.0f, pos.y + 1.0f, pos.z + 0.0f };
-	Vector3 p111 = { pos.x + 1.0f, pos.y + 1.0f, pos.z + 1.0f };
+	Vector3 p0 = { pos.x + 0.0f, pos.y + 0.0f, pos.z + 0.0f };
+	Vector3 p1 = { pos.x + 0.0f, pos.y + 0.0f, pos.z + 1.0f };
+	Vector3 p2 = { pos.x + 0.0f, pos.y + 1.0f, pos.z + 0.0f };
+	Vector3 p3 = { pos.x + 0.0f, pos.y + 1.0f, pos.z + 1.0f };
+	Vector3 p4 = { pos.x + 1.0f, pos.y + 0.0f, pos.z + 0.0f };
+	Vector3 p5 = { pos.x + 1.0f, pos.y + 0.0f, pos.z + 1.0f };
+	Vector3 p6 = { pos.x + 1.0f, pos.y + 1.0f, pos.z + 0.0f };
+	Vector3 p7 = { pos.x + 1.0f, pos.y + 1.0f, pos.z + 1.0f };
 
-	drawFace( p000, p001, p011, p010, RED );	// -X / left face
-	drawFace( p100, p110, p111, p101, ORANGE );	// +X / right face
+	drawFace( p0, p1, p3, p2, RED );	// -X / left face
+	drawFace( p5, p4, p6, p7, ORANGE );	// +X / right face
 
-	drawFace( p000, p100, p101, p001, VIOLET ); // -Y / bottom face
-	drawFace( p010, p011, p111, p110, BLUE );	// +Y / top face
+	drawFace( p5, p1, p0, p4, VIOLET ); // -Y / bottom face
+	drawFace( p3, p7, p6, p2, BLUE );	// +Y / top face
 
-	drawFace( p000, p010, p110, p100, GREEN );	// -Z / back face
-	drawFace( p001, p101, p111, p011, YELLOW );	// +Z / front face
+	drawFace( p4, p0, p2, p6, GREEN );	// -Z / back face
+	drawFace( p1, p5, p7, p3, YELLOW );	// +Z / front face
 }
 
 
@@ -149,19 +143,19 @@ void	drawGeometry( Camera *camera )
 
 	int size = 32;
 
-	for (int x = 0; x < size; x++)
-		for (int y = 0; y < size; y++)
-			for (int z = 0; z < size; z++)
+	for( int x = 0; x < size; x++ )
+		for( int y = 0; y < size; y++ )
+			for( int z = 0; z < size; z++ )
 			{
 				if( x % 2 || y % 2 || z % 2 || ( x + y + z ) % 4 )
 					continue;
-				drawVoxel( ( Vector3 ) { x, y, z } );
+				drawVoxel( ( Vector3 ){ x, y, z } );
 			}
 
 	EndMode3D();
 }
 
-void	drawInfoBox(Camera *camera)
+void	drawInfoBox( Camera *camera )
 {
 	int box_x = 5;
 	int box_y = 5;
